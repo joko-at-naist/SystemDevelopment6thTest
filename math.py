939c6f3420c13a78d10af82f0b295cd244420a11
joko-at-naist <hrjk0410@gmail.com>
2025-12-28 17:13:46 +0900

    Add Math class with absolute function and sign function

---
+++ math.py
@@ -0,0 +1,14 @@
+class Math:
+  def absolute(x):
+    if x < 0:
+      return -x
+    else:
+      return x
+
+def sign(x):
+  if x < 0:
+    return -1
+  elif x > 0:
+    return 1
+  else:
+    return 0
